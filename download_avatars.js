6068cd69fddfb133cd417001d6b837bc4173fc8f
mattwong59 <mattwong59@gmail.com>
2018-03-20 01:56:28 +0000

    Adds printed avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -6,18 +6,21 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors (repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    json: true,
     headers: {
       'User-Agent': 'request',
       'Authorization': secrets.GITHUB_TOKEN
     }
   };
 
-  request (options, function (err, res, body) {
-    cb (err, body);
+  request (options, function (err, res, data) {
+    cb (err, data);
   });
 }
 
 getRepoContributors ("jquery", "jquery", function (err, result) {
   console.log ("Errors:", err);
-  console.log ("Result", result);
+  result.forEach(function(item) {
+      console.log(item.avatar_url);
+    });
 });
